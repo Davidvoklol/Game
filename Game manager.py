390430374c88045a70b163b865950c1f4501737c
Davidvoklol <128652242+Davidvoklol@users.noreply.github.com>
2023-04-07 17:26:33 +0200

    Update Game manager.py

--- Game manager.py
+++ Game manager.py
@@ -2,14 +2,13 @@ import tkinter as tk
 from tkinter import messagebox
 import os
 
-
 window = tk.Tk()
 window.geometry("500x300")
 window.config(bg="gray")
-window.title("Manager")
+window.title("Idle Lobster")
 
 tk.Label(window, text="Game Manager", font=("Consolas", 40, "bold"), bg="gray").pack(fill="x", pady=10)
 
-download = tk.Button(window, text="Download Game", font=("Fixedsys", 20, "bold"), bg="black", activebackground="black", fg="white", activeforeground="white").pack(pady=5)
-delete = tk.Button(window, text="Delete Game", font=("Fixedsys", 20, "bold"), bg="black", activebackground="black", fg="white", activeforeground="white").pack(pady=5)
+download = tk.Button(window, text="DOWNLOAD GAME", font=("Fixedsys", 20, "bold"), bg="black", activebackground="black", fg="white", activeforeground="white").pack(pady=5)
+delete = tk.Button(window, text="DELETE GAME FILES", font=("Fixedsys", 20, "bold"), bg="black", activebackground="black", fg="white", activeforeground="white").pack(pady=5)
 window.mainloop()
