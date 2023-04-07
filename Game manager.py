9a08694ea108878d197bbbbe5c3e4084a54159e5
Davidvoklol <128652242+Davidvoklol@users.noreply.github.com>
2023-04-07 22:39:40 +0200

    Update Game manager.py

--- Game manager.py
+++ Game manager.py
@@ -10,5 +10,6 @@ window.title("Idle Game")
 tk.Label(window, text="Lobster Manager", font=("Consolas", 40, "bold"), bg="gray").pack(fill="x", pady=10)
 
 download = tk.Button(window, text="DOWNLOAD GAME", font=("Fixedsys", 20, "bold"), bg="black", activebackground="black", fg="white", activeforeground="white").pack(pady=5)
+update = tk.Button(window, text="UPDATE GAME", font=("Fixedsys", 20, "bold"), bg="black", activebackground="black", fg="white", activeforeground="white").pack(pady=5)
 delete = tk.Button(window, text="DELETE GAME FILES", font=("Fixedsys", 20, "bold"), bg="black", activebackground="black", fg="white", activeforeground="white").pack(pady=5)
 window.mainloop()
